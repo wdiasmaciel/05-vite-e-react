a751a49e34f2dd5c0fd449df044584ca43a95d8d
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-07-19 21:03:11 +0000

    Exemplo

--- meu-app/src/App.tsx
+++ meu-app/src/App.tsx
@@ -9,3 +9,17 @@ export default function App() {
     </div>
   )
 }
+
+/*
+OBS:
+No framework Tailwind CSS, a classe:
+* antialiased: aplica a propriedade CSS "font-smoothing: antialiased". 
+  Isso melhora a qualidade do texto renderizado, tornando as bordas 
+  dos caracteres mais suaves e menos pixeladas. Ela controla a 
+  suavização de fontes (font smoothing) do elemento. Ela aplica escala 
+  de cinza para suavizar as bordas do texto, deixando as letras mais 
+  nítidas e finas, melhorando a legibilidade. A sua principal vantagem 
+  aparece quando o texto é renderizado no macOS ou em telas Retina, 
+  onde o antialiased previne que fontes pesadas ou finas fiquem com um 
+  aspecto "borrado" ou excessivamente espesso.
+*/
