e6f12858915ddd15e505f6366ef4265085dfa431
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-07-19 09:00:16 +0000

    Exemplo

--- meu-app/src/App.tsx
+++ meu-app/src/App.tsx
@@ -3,7 +3,8 @@ import Painel from './component/painel/Painel'
 export default function App() {
   return (
     <div className="min-h-screen bg-slate-100 p-3 
-                    text-slate-700 antialiased">
+                    text-slate-700 antialiased"
+    >
       <Painel />
     </div>
   )
