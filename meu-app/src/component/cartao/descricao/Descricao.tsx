6bbbabcdbe65c763094ff415b43284f434d57db2
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-07-19 20:37:24 +0000

    Exemplo

--- meu-app/src/component/cartao/descricao/Descricao.tsx
+++ meu-app/src/component/cartao/descricao/Descricao.tsx
@@ -9,3 +9,12 @@ export default function Descricao(props: DescricaoProps) {
     </p>
   )
 }
+
+/*
+OBS:
+No Tailwind CSS, a classe "leading-relaxed" define a altura da 
+linha (espaçamento vertical entre as linhas de um parágrafo) 
+para o valor de 1.625. Isso significa que o espaço entre as linhas 
+será 162,5% do tamanho da fonte, criando um "respiro" maior que 
+melhora a legibilidade, principalmente, em textos longos.
+*/
