42521ad1eb69a910477c0341c78365a3e51116d4
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2026-07-19 21:08:46 +0000

    Exemplo

--- meu-app/src/component/cartao/descricao/Descricao.tsx
+++ meu-app/src/component/cartao/descricao/Descricao.tsx
@@ -4,17 +4,8 @@ interface DescricaoProps {
 
 export default function Descricao(props: DescricaoProps) {
   return (
-    <p className="text-sm leading-relaxed text-slate-600">
+    <p className="text-sm text-slate-600">
       {props.texto}
     </p>
   )
 }
-
-/*
-OBS:
-No Tailwind CSS, a classe "leading-relaxed" define a altura da 
-linha (espaçamento vertical entre as linhas de um parágrafo) 
-para o valor de 1.625. Isso significa que o espaço entre as linhas 
-será 162,5% do tamanho da fonte, criando um "respiro" maior que 
-melhora a legibilidade, principalmente, em textos longos.
-*/
